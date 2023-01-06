d7539f7660a6212faf07aeb595072a44836f2dab
Redzovic Iljaz <hvalavitalisu@gmail.com>
2023-01-06 04:33:17 +0100

    first

---
+++ funkcije.js
@@ -0,0 +1,124 @@
+function spusti(term_in){
+    //term_in = document.createElement('div');
+
+    inputValue = document.createElement("label");
+    for(let i = 0; i < nizK.length; i++){
+        zagrade[i] = document.createElement("label");
+        zagrade[i].innerHTML = nizK[i];
+        zagrade[i].style.color = nizB[i];
+        term_in.appendChild(zagrade[i]);
+    }
+    //term_pre.appendChild(PS1);
+    inputValue.innerHTML = _input.value;
+    inputValue.style.color = "white";
+    inputValue.className = 'prosli';
+    term_in.appendChild(inputValue);
+    term_pre.appendChild(term_in);
+}
+function _help(){
+    let hel = new Array(nizhelp.length);
+    term_in = document.createElement('div');
+    let temmp = document.createElement('label');
+    spusti(term_in);
+    temmp.innerHTML = '<br>';
+    term_in.appendChild(temmp);
+    for(let i = 0; i < hel.length; i++){
+        hel[i] = document.createElement("label");
+        hel[i].innerHTML = nizhelp[i] + ' &nbsp &nbsp -- &nbsp &nbsp' + nizhelpA[i] + '<br>';
+        hel[i].style.color = 'white';
+        term_in.appendChild(hel[i]);
+    }
+    term_pre.appendChild(term_in);
+    input.value = '';
+}
+function _clear(){
+    term_pre.innerHTML = '';
+    input.value = '';
+    console.clear();
+}
+function _fortune(){
+    term_in = document.createElement('div');
+    _output = document.createElement("p");
+    var rand_index = Math.floor(Math.random() * quotes.length);;
+    var q = quotes[rand_index];
+    if(!(input.value.match(/cowsay/g))){
+        spusti(term_in);
+        _output.innerHTML = q;
+        term_in.appendChild(_output);
+        term_pre.appendChild(term_in);
+    }
+    else return q;
+   // console.log(q);
+    input.value = '';
+}
+function _cowsay(){
+    inputValue = input.value.replace(/^cowsay/g, '');
+    term_in = document.createElement('div');
+    let okolo = document.createElement('div');
+    let quote = document.createElement('p');
+    let krava = document.createElement('label');
+    okolo.className = 'cow';
+    //console.log(inputValue);
+    spusti(term_in);
+    if(input.value.match(/\| fortune/g)){
+        quote.innerHTML = _fortune();
+    }
+    else quote.innerHTML = inputValue;
+    krava.innerHTML = cowsay;
+    krava.className = 'zapravo_krava';
+    okolo.appendChild(quote);
+    term_in.appendChild(okolo);
+    term_in.appendChild(krava);
+    term_pre.appendChild(term_in);
+
+
+    input.value = '';
+}
+function _echo() {
+    term_in = document.createElement('div');
+    _output = document.createElement("p");
+    spusti(term_in);
+    inputValue = input.value;
+    if(inputValue.match(/^printf/)) 
+        console.log(inputValue.replace(/^printf/, ''));
+    else if(inputValue.match(reg))
+        _output.innerHTML = inputValue.replace(reg, '');
+    term_in.appendChild(_output);
+    term_pre.appendChild(term_in);
+    input.value = '';
+
+}
+
+function greska(){
+    term_in = document.createElement('div');
+    _output = document.createElement("p");
+
+    _output.innerHTML = "command not found: "  + input.value;
+    spusti(term_in);
+    term_in.appendChild(_output);
+    term_pre.appendChild(term_in);
+    input.value = '';
+}
+function _whoami(){
+    let term_in = document.createElement('div');
+    _output = document.createElement("p");
+    spusti(term_in);
+    _output.innerHTML = 'ja sam taj';
+    
+    term_in.appendChild(_output);
+    term_pre.appendChild(term_in);
+    _input.value = '';
+}
+function _kontakt(naziv_linka, link){
+    let term_in = document.createElement('div');
+    spusti(term_in);
+    _output = document.createElement('a');
+    let temp = document.createElement('br');
+    let linkTemp = document.createTextNode(naziv_linka);
+    _output.appendChild(linkTemp);
+    _output.href = link;
+    term_in.appendChild(temp);
+    term_in.appendChild(_output);
+    term_pre.appendChild(term_in);
+    input.value = '';
+}
---
+++ quotes.js
@@ -0,0 +1,22 @@
+const quotes = [
+    "The only way to do great work is to love what you do. If you haven't found it yet, keep looking. Don't settle. As with all matters of the heart, you'll know when you find it. - Steve Jobs",
+    "Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work. And the only way to do great work is to love what you do. If you haven't found it yet, keep looking. Don't settle. - Steve Jobs",
+    "Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful. - Albert Schweitzer",
+    "Happiness is not something ready made. It comes from your own actions. - Dalai Lama",
+    "Happiness is not something you postpone for the future; it is something you design for the present. - Jim Rohn",
+    "Patience is a minor form of despair, disguised as virtue.-- Ambrose Bierce, on qualifiers",
+    "Bowie's Theorem:   If an experiment works, you must be using the wrong equipment.",
+    "A bachelor is a selfish, undeserving guy who has cheated some woman out of a divorce. -- Don Quinn",
+    "Your wig steers the gig. -- Lord Buckley",
+    "The computer is to the information industry roughly what the central power station is to the electrical industry.  -- Peter Drucker ",
+    " Is your job running? You'd better go catch it!",
+    "The very ink with which all history is written is  merely fluid prejudice. Mark Twain",
+    "Majstore, šta ti je ovo?"
+  ];
+  const cowsay = `
+        \\   ^__^ <br> &nbsp
+         \\  (oo)\\_______ <br> &nbsp&nbsp&nbsp&nbsp
+            (__)\\&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp)\\/\\ <br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+           &nbsp||----w | <br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                || &nbsp&nbsp&nbsp&nbsp&nbsp    || <br>
+`;
